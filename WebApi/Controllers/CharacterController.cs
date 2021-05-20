00ea853a186b5186517eab515421a676338bb250
Jackson Miller <jacksonmiller7855@gmail.com>
2021-05-19 22:13:34 -0400

    fix issue where not all character routes worked

--- WebApi/Controllers/CharacterController.cs
+++ WebApi/Controllers/CharacterController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace WebApi.Controllers
 {
+    [Route("character")]
     public class CharacterController : ControllerBase
     {
         private readonly ICharacterService _service;

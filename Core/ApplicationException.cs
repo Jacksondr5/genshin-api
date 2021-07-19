2bba9d9d28a5333e34800d731b0978a5dce65a01
Jackson Miller <jacksonmiller7855@gmail.com>
2021-07-18 23:28:01 -0400

    fix sonarqube issues

--- Core/ApplicationException.cs
+++ Core/ApplicationException.cs
@@ -1,11 +1,20 @@
 using System;
+using System.Runtime.Serialization;
 
 namespace Core
 {
+    [Serializable]
     public class GenshinException : Exception
     {
         public GenshinException(string? message) : base(message)
         {
+        }
+        public override void GetObjectData(
+            SerializationInfo info,
+            StreamingContext context
+        )
+        {
+            base.GetObjectData(info, context);
         }
     }
 }
